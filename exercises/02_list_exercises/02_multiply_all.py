decf60b48eb991e8a920f2468d9900be583c5682
wooihaw <wooihaw.tan@gmail.com>
2023-04-04 13:19:40 +0800

    Updated with solutions

--- exercises/02_list_exercises/02_multiply_all.py
+++ exercises/02_list_exercises/02_multiply_all.py
@@ -1,3 +1,8 @@
 # Write a Python script to multiply all the items in a list.
 alist = [12, 43, 57, 98, 83, -103, 256, -1]
 print(f'{alist=}')
+
+prod_list = 1
+for i in alist:
+    prod_list *= i
+print(f"The product is {prod_list}")
